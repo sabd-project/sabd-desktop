2c922c3ec49cf3bf84e31aa93beb90a3ea9b41c6
Jujhar Singh <jujhar@jujhar.com>
2016-03-12 17:31:07 +0000

    parked

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let mainWindow;
 function createWindow() {
     // Create the browser window.
     mainWindow = new BrowserWindow({
-        width: 800, height: 600, 'min-width': 500,
+        width: 900, height: 600, 'min-width': 500,
         'min-height': 200,
         'accept-first-mouse': true,
         'title-bar-style': 'hidden'

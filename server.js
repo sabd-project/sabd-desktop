d262c6679edc508ea5b9d03ec886302891b983cd
Jujhar Singh <jujhar@jujhar.com>
2016-03-15 20:38:08 +0000

    parking, gotta practice

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
+app.get('/search/:type/:query', function (req, res) {
+
+
+    res.send('Hello World!' + req.params.type);
+});
+
 app.listen(PORT, function () {
-    console.log('Example app listening on port !' + PORT);
+    console.log('Example app listening on TCP:' + PORT);
 });

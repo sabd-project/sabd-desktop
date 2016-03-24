c7add04acd31a079a45ed632c56fd27fa3ee6f64
Jujhar Singh <jujhar@jujhar.com>
2016-03-24 20:00:01 +0000

    working on frontend -- badly

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,57 @@
-
-function ready(fn) {
-    if (document.readyState != 'loading'){
-        startup();
-    } else {
-        document.addEventListener('DOMContentLoaded', startup);
-    }
-}
+"use strict";
 
 var app = app || {};
 const ENTER_KEY = 13;
 const ESC_KEY = 27;
+const API_URL = "http://localhost:9102";
 
-function startup(){
-    "use strict";
+window.onload = function () {
 
-    $('input#search').onkeydown(function(e){
+    //bind to search field and button
+    var searchField = $('input#search');
+    var searchButton = $('button#searchButton');
+    var searchType = 'fls';
 
-        if ($(e).keyCode===ENTER_KEY){
-            console.log(e);
+    searchField.keydown(function (e) {
+
+        if (searchField.val().length > 0) {
+            searchButton.prop('enabled', true);
+            if (e.keyCode === ENTER_KEY) {
+                search(searchField.val(), searchType);
+            }
+        }
+        else {
+            searchButton.prop('disabled', true);
         }
     });
+
+    searchButton.click(function (e) {
+        search(searchField.val(), searchType);
+    });
+
+};
+
+/**
+ * search for gurbani
+ *
+ * @param value
+ * @param searchType
+ */
+function search(value, searchType) {
+    console.debug('Searching for', value);
+
+    jQuery.getJSON({
+        url: API_URL + "/search/" + searchType + "/" + value,
+        success: function (data) {
+            renderResults(data);
+        }
+    });
+}
+
+function renderResults(data) {
+    console.debug(data);
+    var source = $("#tpl-search").html();
+    var template = Handlebars.compile(source);
+    var html = template(data);
+    console.log(html)
 }
